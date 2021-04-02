9f46e7eb65fd8230483a973d7534fefc4d83627c
Charlie <belinsky@msu.edue>
2021-04-02 08:07:15 -0400

    added test folder with data frame

--- scripts/1-02_DataFrames.R
+++ scripts/1-02_DataFrames.R
@@ -1,4 +1,4 @@
-{
+{ # all scripts will start and end with a curly bracket
   rm(list=ls());                        # Clean the Environment tab
   options(show.error.locations = TRUE); # Show line # on errors
   
@@ -13,8 +13,10 @@
   weatherData2 = read.csv(file = "data/LansingNOAA2016.csv");
   
   # weatherData3 is how weatherData2 looks for people using R version 3 
-  weatherData3 = read.csv(file = "data/LansingNOAA2016.csv",
-                          stringsAsFactors = TRUE);
+  # Uncomment the next two lines, execute, and expand in the Environment to
+  # see the difference -- we will talk more about this in future lessons.
+  # weatherData3 = read.csv(file = "data/LansingNOAA2016.csv",
+  #                         stringsAsFactors = TRUE);
   
   # Pull out the maxTemp column from weatherData and save it to a 
   # variable named highTemps
@@ -34,18 +36,18 @@
   valComb = highTemps[c(25,158,273)];
   
   # Getting consecutive values (131 throuugh 147) from the highTemps vector
-  consecVals = weatherData$maxTemp[131:147];
+  consecVals = highTemps[131:147];
   
   # Getting the consecutive value in reverse
-  consecValsRev = weatherData$maxTemp[147:131];
+  consecValsRev = highTemps[147:131];
   
   # Get every other value (by=2) from highTemps between 131 and 147
-  seq1 = weatherData$maxTemp[seq(from=131, to=147, by=2)];
+  seq1 = highTemps[seq(from=131, to=147, by=2)];
   
-  # GEt every other value in reverse
-  seq2 = weatherData$maxTemp[seq(from=147, to=131, by=-2)];
+  # Get every other value in reverse
+  seq2 = highTemps[seq(from=147, to=131, by=-2)];
   
   # Get every seventh value from the highTemps
-  seq3 = weatherData$maxTemp[seq(from=1, to=366, by=7)];
+  seq3 = highTemps[seq(from=1, to=366, by=7)];
   
 }   # matches curly bracket on first line
