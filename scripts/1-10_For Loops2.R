35c7bb6884b87f4b7f88fc1b097371ed5cd1becf
Charlie <belinsky@msu>
2021-07-28 16:43:56 -0400

    1-9 and 1-10 separated

--- scripts/1-10_For Loops2.R
+++ scripts/1-10_For Loops2.R
@@ -71,4 +71,34 @@
     }
     # there is no else here -- we don't care about non-sunny days
   }
+  
+  # # if-any
+  # anyTornados = FALSE;  # often called state variables
+  # anyFog = FALSE;
+  # 
+  # cloudyDays = 0;
+  # for(i in 1:highTempsL)
+  # {
+  #   if(noonCond[i] == "Fog")
+  #   {
+  #     anyFog = TRUE;
+  #   }   
+  #   if(noonCond[i] == "Blizzard")
+  #   {
+  #     anyBlizzard = TRUE; 
+  #   }
+  # }
+  # # mention inefficiency of checking after values have been found
+  # cat("The number of cloudy days was: ", cloudyDays, "\n");
+  # 
+  # # go through only even values - note why you don't want to use 14:
+  # evenNum = seq(from=1, by=2, to=length(highTempsL));
+  # 
+  # for(i in evenNum)
+  # {
+  #   if(noonCond[i] == "Cloudy")
+  #   {
+  #     cloudyDays = cloudyDays +1; 
+  #   }   
+  # }
 }
