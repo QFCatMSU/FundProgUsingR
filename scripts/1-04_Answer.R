c0a14ec6e7dfa3931a55508c488b4f1b76f2ecc9
Charlie <belinsky@msu>
2021-09-09 15:51:47 -0400

    fixed 1-4

--- scripts/1-04_Answer.R
+++ scripts/1-04_Answer.R
@@ -30,4 +30,12 @@
   seq1 = highTemps[seq(from=1, to=14, by=2)];   # values 1,3,5,7,9,11,13
   seq2 = highTemps[seq(from=14, to=1, by=-2)];  # values 14,12,10,8,6,4,2
   seq3 = highTemps[seq(from=1, to=10, by=3)];   # values 1,4,7,10
+  
+  
+  dowJonesData = read.csv(file="data/DowJones1980_2012.csv", 
+                          sep=" ",
+                          header=FALSE, 
+                          stringsAsFactors = FALSE);  
+  
+  plot(dowJonesData$V2);
 }
