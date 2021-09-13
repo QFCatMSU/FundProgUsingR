425aba92d86ef65f3b523625b63110ad4cdc4784
Charlie <belinsky@msu>
2021-09-13 12:03:18 -0400

    Fixed 1.4 to break up sequence functions

--- scripts/1-04_CSV_Files_and_Data_Frames.R
+++ scripts/1-04_CSV_Files_and_Data_Frames.R
@@ -26,8 +26,20 @@
   consecVals = highTemps[3:11];     
   consecValsRev = highTemps[11:3]; # in reverse
   
-  # Use the sequence function to subset the highTemps
-  seq1 = highTemps[seq(from=1, to=14, by=2)];   # values 1,3,5,7,9,11,13
-  seq2 = highTemps[seq(from=14, to=1, by=-2)];  # values 14,12,10,8,6,4,2
-  seq3 = highTemps[seq(from=1, to=10, by=3)];   # values 1,4,7,10
+  # Create a vector with sequenced numbers 
+  seq1 = seq(from=1, to=14, by=2);   # 1,3,5,7,9,11,13
+  seq2 = seq(from=14, to=1, by=-2);  # 14,12,10,8,6,4,2
+  seq3 = seq(from=1, to=10, by=3);   # 1,4,7,10
+
+  # Use the sequenced indexes to subset the highTemps
+  highTempSeq1 = highTemps[seq1];   # values 1,3,5,7,9,11,13
+  highTempSeq2 = highTemps[seq2];   # values 14,12,10,8,6,4,2
+  highTempSeq3 = highTemps[seq3];   # values 1,4,7,10
+  
+  # You could combine the two previous steps into 1 step:
+  highTempSeq1b = highTemps[seq(from=1, to=14, by=2)];  
+  highTempSeq2b = highTemps[seq(from=14, to=1, by=-2)];  
+  highTempSeq3b = highTemps[seq(from=1, to=10, by=3)];
+  
+  
 }
