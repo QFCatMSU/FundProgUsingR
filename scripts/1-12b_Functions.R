e9bf0573bebb135b29bcad7e94da22d239aab7c2
Charlie <Charlie>
2021-11-23 07:07:04 -0500

    Lesson 13b

--- scripts/1-12b_Functions.R
+++ scripts/1-12b_Functions.R
@@ -2,138 +2,56 @@
   rm(list=ls());  # cleans out the Environment every time the code is executed
   options(show.error.locations = TRUE);  # show the line number of errors in the Console
   
+  source(file="scripts/temperature.r");
+  
+  cat1a = tempCategory(45);
+  cat1b = tempCategory(tempValue=85);
+  cat1c = tempCategory(tempValue=17);
+  cat1d = tempCategory(-2); 
+  
+  cat2a = tempCategory2(45);
+  cat2b = tempCategory2(15, "F");
+  cat2c = tempCategory2(15, "C");
+  cat2d = tempCategory2(tempValue=35, unit="C");
+  cat2e = tempCategory2(tempValue=45, unit="C");  # NULL because 45C > 100F
+  
+  
+  cat3a = tempCategory3(45);
+  cat3b = tempCategory3(15, TRUE);
+  cat3c = tempCategory3(15, FALSE);
+  cat3d = tempCategory3(tempValue=35, isFahr=FALSE);
+  cat3e = tempCategory3(tempValue=45, isFahr=FALSE);
+  
+  cat4a = tempCategory4(305, "K");
+  cat4b = tempCategory4(270, "K");  
+  cat4c = tempCategory4(285, "K");
+  cat4d = tempCategory4(tempValue=325, unit="K");
+  
+  cat5a = tempCategory5(45);
+  cat5b = tempCategory5(tempVector=85);
+  cat5c = tempCategory5(tempVector=17);
+  cat5d = tempCategory5(-2);
+  
+  cat5e = tempCategory5(c(10,30,50,70,90));
+  cat5f = tempCategory5(seq(from=5, to=90, by=20));
+  cat5g = tempCategory5(tempVector=c(30,25,20,15,10), unit="C");
+  cat5h = tempCategory5(tempVector=seq(from=25, to=-5, by=-5), unit="C");
+  
+  
   ### read in data from  twoWeekWeatherData.csv
   weatherData = read.csv(file="data/LansingNOAA2016.csv", 
                          sep=",",
                          header=TRUE, 
                          stringsAsFactors = FALSE);  
-  
-  windSpeedCategory = function(speed)
-  {
-    if(speed == 0)
-    {
-      return("none");
-    }
-    else if(speed > 0 & speed <= 10)
-    {
-      return("light");
-    }
-    else if(speed > 10 & speed <= 25)
-    {
-      return("moderate");
-    }
-    else if(speed > 25 & speed <= 40)
-    {
-      return("strong");
-    }
-    else if(speed > 40)
-    {
-      return("gale");
-    }
-  }
-  singleVal1 = windSpeedCategory(45);
-  singleVal2 = windSpeedCategory(8);
-  singleVal4 = windSpeedCategory(-4);  # note the NULL
-  singleVal3 = windSpeedCategory(23);
 
-  windSpeedCategory2 = function(speed)
-  {
-    categoryVec = c();
-    for(i in 1:length(speed))
-    {
-      if(speed[i] == 0)
-      {
-        categoryVec[i] = "none";
-      }
-      else if(speed[i] > 0 & speed[i] <= 10)
-      {
-        categoryVec[i] = "light";
-      }
-      else if(speed[i] > 10 & speed[i] <= 25)
-      {
-        categoryVec[i] = "moderate";
-      }
-      else if(speed[i] > 25 & speed[i] <= 40)
-      {
-        categoryVec[i] = "strong";
-      }
-      else if(speed[i] > 40)
-      {
-        categoryVec[i] = "gale";
-      }
-    }
-    return(categoryVec);
-  }
-  multValues = windSpeedCategory2(c(45,8,-4,23));  # note the NA
-  windSpeeds = weatherData$windPeakSpeed;
-  multValues = windSpeedCategory2(windSpeeds);
+  cat5i = tempCategory5(weatherData$maxTemp);
+  cat5j = tempCategory5(weatherData$minTemp);
   
-  windSpeedCategory3 = function(speed, miles_hour = TRUE)
-  {
-    if(miles_hour == FALSE)
-    {
-      speed = speed *10; 
-    }
-    categoryVec = c();
-    for(i in 1:length(speed))
-    {
-      if(speed[i] == 0)
-      {
-        categoryVec[i] = "none";
-      }
-      else if(speed[i] > 0 & speed[i] <= 10)
-      {
-        categoryVec[i] = "light";
-      }
-      else if(speed[i] > 10 & speed[i] <= 25)
-      {
-        categoryVec[i] = "moderate";
-      }
-      else if(speed[i] > 25 & speed[i] <= 40)
-      {
-        categoryVec[i] = "strong";
-      }
-      else if(speed[i] > 40)
-      {
-        categoryVec[i] = "gale";
-      }
-    }
-    return(categoryVec);
-  }
-  multValues2 = windSpeedCategory3(c(45,8,-4,23), FALSE);  # note the NA
-  multValues2 = windSpeedCategory3(c(45,8,-4,23), TRUE);  # note the NA
-  multValues2b = windSpeedCategory3(windSpeeds);
+  # add the vectors to the data frame
+  weatherData$maxTempCat = cat5i;
+  weatherData$minTempCat = cat5j;
   
-  windSpeedCategory4 = function(speed, units)  #just 2
-  {
-    if(units == "m_s")
-    {
-      speed = speed *10; 
-    }
-    categoryVec = c();
-    for(i in 1:length(speed))
-    {
-      if(speed[i] == 0)
-      {
-        categoryVec[i] = "none";
-      }
-      else if(speed[i] > 0 & speed[i] <= 10)
-      {
-        categoryVec[i] = "light";
-      }
-      else if(speed[i] > 10 & speed[i] <= 25)
-      {
-        categoryVec[i] = "moderate";
-      }
-      else if(speed[i] > 25 & speed[i] <= 40)
-      {
-        categoryVec[i] = "strong";
-      }
-      else if(speed[i] > 40)
-      {
-        categoryVec[i] = "gale";
-      }
-    }
-    return(categoryVec);
-  }
+  # double-click on weatherData in the Environment to see these vectors
+  #   they will be the last two columns
+  
 }
