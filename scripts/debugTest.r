835b1d70d8bb16c4066697e3b4e20b774d1f6357
belinskyc <36079242+belinskyc@users.noreply.github.com>
2022-10-03 09:07:46 -0400

    lesson 2-12 scripts

---
+++ scripts/debugTest.r
@@ -0,0 +1,44 @@
+### PAckages:
+# devtools needs to be installed/included
+#library(devtools);
+# package is added
+#devtools::install_github("ChrisFishCahill/gg-qfc", force=TRUE);
+#library(ggqfc);
+#hello()
+#tester()
+
+convertTime = function(time_24Hour)
+{
+  # break the 4 digit number into hours and minutes
+  hour = floor(time_24Hour/100); 
+  minutes = time_24Hour %% 100;
+  ampm = NULL;
+  
+  if(hour == 0)
+  {
+    hour = 12;
+    ampm = "am"
+  }
+  else if(hour < 12)
+  {
+    ampm = "am"
+  }
+  else if(hour == 12)
+  {
+    ampm = "pm" 
+  }
+  else if(hour > 12)
+  {
+    hour = hour - 12; 
+    ampm = "pm";
+  }
+
+
+  if (minutes < 10)
+  {
+    minutes = paste("0", minutes, sep=""); 
+  }
+  time_12Hour = paste(hour, ":", minutes, ampm, sep="");
+  return(time_12Hour)
+}
+
