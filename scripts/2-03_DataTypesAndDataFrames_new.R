95a1484c7389476fefc9d59b3e100020965d1e5c
belinskyc <belinsky@msu.edu>
2025-06-06 10:22:53 -0400

    Fixed lesson 2-03

--- scripts/2-03_DataTypesAndDataFrames_new.R
+++ scripts/2-03_DataTypesAndDataFrames_new.R
@@ -4,15 +4,14 @@ library(package=ggplot2);
 ### First let's read in the bad csv file:
 badData = read.csv(file="data/Lansing2016Noaa-2-bad.csv",
                        sep=",",
-                       header=TRUE, 
-                       stringsAsFactors = FALSE);
+                       header=TRUE);
 
 ### Keep writing with row.names then you can have multiple X columns
 
 ### Adding/modifying columns with incorrect number of values
-## badData$test1 = c(1:400); 
-## badData$test2 = c(1:10);
-## The above lines will give an error
+# badData$test1 = c(1:400); 
+# badData$test2 = c(1:10);
+# The above lines will give an error
 
 
 ### Unless the number of values evenly divides the number of row in the data frame
@@ -30,14 +29,14 @@ firstThreeMonths = c(rep("Jan", 31), rep("Feb", 29), rep("Mar", 31));
 ### Now let's read to good data frame
 weatherData = read.csv(file="data/Lansing2016Noaa-2.csv",
                        sep=",",
-                       header=TRUE, 
-                       stringsAsFactors = FALSE);
+                       header=TRUE);
 
 ### Let's grab the precip column 
 precip = weatherData$precip;
 
-### Mathematical operations on a string column will cause an error
+### Mathematical operations on a string column will return an error
 #  totalPrecip = sum(precip);
+### Statistical operations on a string column will return NA
 #  meanPrecip = mean(precip);
 
 # Create a numeric version of precip -- call it precip2
@@ -70,7 +69,7 @@ for(i in 1:length(precip3))
 meanPrecip3 = mean(precip3);
 totalPrecip3 = sum(precip3); 
 
-### SAve the precip3 vector as the column precipNum in weatherData
+### Save the precip3 vector as the column precipNum in weatherData
 weatherData$precipNum = precip3;
 
 ### Write the current data frame to a new CSV file -- this will be used next lesson
@@ -89,29 +88,6 @@ plot1 = ggplot(data=weatherData ) +
   theme_bw();
 plot(plot1);
 
-# NAs introduced by coercion... (NA values not plotted)
-plot(x=1:nrow(weatherData),  # 366
-     y=weatherData$precip,
-  #   type = "n",  # don't draw points
-     main = "Humidity vs Temperature\nLansing, MI -- 2016",
-     col.main = "red",
-     xlab = "Average Temperatures (Fahrenheit)", 
-     ylab = "Relative Humidity",
-     col.lab="green",
-     xaxt = 'n');
-
-# 
-plot(x=1:nrow(weatherData),  # 366
-     y=weatherData$precipNum,
-     #   type = "n",  # don't draw points
-     main = "Humidity vs Temperature\nLansing, MI -- 2016",
-     col.main = "red",
-     xlab = "Average Temperatures (Fahrenheit)", 
-     ylab = "Relative Humidity",
-     col.lab="green",
-     xaxt = 'n');
-
-
 ## Scatterplot using the precip column with chr values --
 #  This lists every precip value with T at top since T is > all #s)
 plot2 = ggplot(data=weatherData ) +
@@ -124,3 +100,27 @@ plot2 = ggplot(data=weatherData ) +
         y = "Precipitation (inches)") +
   theme_bw();
 plot(plot2);
+
+
+##### base-R plots
+# # NAs introduced by coercion... (NA values not plotted)
+# plot(x=1:nrow(weatherData),  # 366
+#      y=weatherData$precip,
+#      #   type = "n",  # don't draw points
+#      main = "Humidity vs Temperature\nLansing, MI -- 2016",
+#      col.main = "red",
+#      xlab = "Average Temperatures (Fahrenheit)", 
+#      ylab = "Relative Humidity",
+#      col.lab="green",
+#      xaxt = 'n');
+# 
+# # 
+# plot(x=1:nrow(weatherData),  # 366
+#      y=weatherData$precipNum,
+#      #   type = "n",  # don't draw points
+#      main = "Humidity vs Temperature\nLansing, MI -- 2016",
+#      col.main = "red",
+#      xlab = "Average Temperatures (Fahrenheit)", 
+#      ylab = "Relative Humidity",
+#      col.lab="green",
+#      xaxt = 'n');
