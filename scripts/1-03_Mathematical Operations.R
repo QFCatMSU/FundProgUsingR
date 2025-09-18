84bed43a1f9e59db393412e302ea3c1aeccda21a
belinskyc <belinsky@msu.edu>
2025-09-18 13:23:37 -0400

    Multiple minor changes to early lessons

--- scripts/1-03_Mathematical Operations.R
+++ scripts/1-03_Mathematical Operations.R
@@ -1,5 +1,5 @@
 rm(list=ls());        # clear out the Environment  
-options(show.error.locations = TRUE); # give line number of error*
+options(show.error.locations = TRUE); # give line number of error
 
 finalDistance = 100;
 initDistance = 50;
