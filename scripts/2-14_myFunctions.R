00466a35921a2df13730d1ba9bbe2409db9b6920
belinskyc <belinsky@msu.edu>
2026-01-15 15:12:41 -0500

    various fixes

--- scripts/2-14_myFunctions.R
+++ scripts/2-14_myFunctions.R
@@ -33,42 +33,53 @@ isprime(c(1,2,4,71,88,2131, 3287, 7819));
 
 high_and_low = function(highs, lows, dates)
 {
+  # initialize all to the first value
+  highest_high = highest_low = list();
+  highest_high$temp = highest_low$temp = highs[1];  
+  highest_high$date = highest_low$date = dates[1];  
+
+  lowest_high = lowest_low = list();
+  lowest_high$temp = lowest_low$temp = highs[1];  
+  lowest_high$date = lowest_low$date = dates[1];  
   
-  highest_high = 0;  
-  highest_low = 0;
-  lowest_high = 0;
-  lowest_low = 0;
+
   
-  for(i in 1:length(highs))
+  ### Add code to handle multiple dates that have the same extreme temp
+  for(i in 2:length(highs))
   {
-    if(highs[i] > highest_high)
+    if(highs[i] > highest_high$temp)
     {
-      highest_high = highs[i];
+      highest_high$temp = highs[i];
+      highest_high$date = dates[i];
       attr(highest_high, "date") = dates[i];
     }
-    else if(highs[i] < lowest_high)
+    else if(highs[i] < lowest_high$temp)
     {
-     lowest_high = highs[i];
-     attr(lowest_high, "date") = dates[i];
-    }
-    else if(lows[i] > highest_low)
-    {
-      lowest_high = lows[i];
+      lowest_high$temp = highs[i];
+      lowest_high$date = dates[i];
       attr(lowest_high, "date") = dates[i];
     }
-    else if(lows[i] < lowest_low)
+    else if(lows[i] > highest_low$temp)
     {
-      lowest_low = lows[i];
+      highest_low$temp = lows[i];
+      highest_low$date = dates[i];
+      attr(highest_low, "date") = dates[i];
+    }
+    else if(lows[i] < lowest_low$temp)
+    {
+      lowest_low$temp = lows[i];
+      lowest_low$date = dates[i];
       attr(lowest_low, "date") = dates[i];
     }
   }
-  
-  vals = c(highest_high, highest_low, lowest_high, lowest_low);
-  
-  names(vals) = c("highest high", "highest low", "lowest high", "lowest low");
-  
-  #### return a list instead??
-  return(vals)
+
+  extreme_temp = list();
+  extreme_temp[["highest_high"]] = highest_high;
+  extreme_temp[["highest_low"]] = highest_low;
+  extreme_temp$lowest_high = lowest_high;
+  extreme_temp$lowest_low = lowest_low;
+
+  return(extreme_temp)
 
 }
 
