ff0160a9eb7adf712ae340a13d6bdb30412e6c23
belinskyc <belinsky@msu.edu>
2026-02-08 19:55:34 -0500

    Added which() to datetime lesson

--- docs/scripts/2-06_DateTimeObjects_new.R
+++ docs/scripts/2-06_DateTimeObjects_new.R
@@ -33,6 +33,26 @@ date_formatted = format(stnDate, format="%m-%d-%y"); # this is a chr vector -- n
 ### Or, just pull info from it
 date_weekOfDay = format(stnDate, format="%A");       # this is a chr vector
 
+### We can use which to subset a Date vector:
+#   3 different ways to subset all April values
+apr_dates1 = which(format(stnDate, "%m") == "04");    # by month number (as character)
+apr_dates2 = which(format(stnDate, "%b") == "Apr");   # by abbreviated month name
+apr_dates3 = which(format(stnDate, "%B") == "April"); # by full month name
+
+### Subset with multiple conditions:
+# april or may
+apr_may_dates = which(format(stnDate, "%b") == "Apr" | 
+                      format(stnDate, "%b") == "May");
+
+# april and 15
+apr_15_dates = which(format(stnDate, "%b") == "Apr" & 
+                     format(stnDate, "%d") == 15);
+
+# april and day > 10 & day < 19 
+apr_10_19_dates = which(format(stnDate, "%b") == "Apr" & 
+                        format(stnDate, "%d") > 10 &
+                        format(stnDate, "%d") < 19 );
+
 # dateTime1 column in pseudoData looks like this: 2022-04-15 09:56PM
 # broken down there is:
 #    - the 4-digit year (%Y)
@@ -45,6 +65,8 @@ date_weekOfDay = format(stnDate, format="%A");       # this is a chr vector
 #    - a colon
 #    - minutes (%M)
 #    - AM/PM indicator (%p)
+
+
 
 ### With date-time columns, we need to use POSIXct
 ##    POSIXct is the UNIX standard for date-times (developed in the 1970s)
