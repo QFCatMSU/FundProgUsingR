59621c441d11ec37ad8cce6b83e68b17278468bc
belinskyc <belinsky@msu.edu>
2026-04-27 14:52:46 -0400

    Setting up for new version of class

---
+++ scripts/lesson1-02.r
@@ -0,0 +1,9 @@
+rm(list=ls());
+# this code sometimes gives the line number of an error
+options(show.error.locations = TRUE);  
+
+# create three variables: d, t, and v
+# give d and t values and use them to calculate v
+d = 100;
+20 = t;   # error!
+v = d/t;  # this line will not be executed
