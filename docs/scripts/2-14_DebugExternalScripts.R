71c17997b7987ef4da88652bf8c46b7aa5a9a6d3
belinskyc <belinsky@msu.edu>
2026-08-10 15:58:07 -0400

    Breakpoints lesson started (2-14)

--- docs/scripts/2-14_DebugExternalScripts.R
+++ docs/scripts/2-14_DebugExternalScripts.R
@@ -1,6 +1,10 @@
 rm(list=ls());
 source("scripts/2-14_myFunctions.R");
 
+#download.packages("pracma", destdir = ".", type = "source")
+#untar("pracma_x.y.z.tar.gz")
+devtools::load_all("~/pracma")
+
 # read in CSV file and save the content to weatherData
 weatherData = read.csv(file="data/Lansing2016NOAA-3.csv");
 
@@ -8,9 +12,4 @@ temps = high_and_low(weatherData$maxTemp,
                      weatherData$minTemp, 
                      weatherData$dateYear);
 
-
-## load the local version of the pracma package
-pkgload::load_all("scripts/pracma")
-
-## Call isprime 
-isprime(c(1,2,4,71,88,2131, 3287, 7819));
+p = pracma::isprime(c(1,2,4,71,88,2131, 3287, 7819))
